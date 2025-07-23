ed033daef97578f64a3cc1744271a56db9060e72
Willian Lopes <contato.willianlopes1@gmail.com>
2025-07-22 21:54:50 -0300

    Erros do ESLint corrigidos

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,24 @@
+import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import json from "@eslint/json";
-import { defineConfig } from "eslint/config";
-
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
+
+  // ✅ Regras personalizadas da aula
+  {
+    files: ["**/*.{js,ts}"],
+    rules: {
+      indent: ["error", 2],                // 2 espaços
+      quotes: ["error", "double"],         // aspas duplas
+      semi: ["error", "always"],           // ponto e vírgula obrigatório
+      "linebreak-style": ["error", "windows"]
+    }
+  },
+
   tseslint.configs.recommended,
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] }
 ]);
