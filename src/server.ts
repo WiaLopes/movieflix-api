b80d75261fb685c4c77ef1c4514e12e6958ee64f
Willian Lopes <contato.willianlopes1@gmail.com>
2025-09-30 21:37:58 -0300

    adicionada remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -79,12 +79,31 @@ app.put("/movies/:id", async (req, res) => {
             },
             data: data
         });
-    }catch(error) {
+    } catch (error) {
         return res.status(500).send({ message: "Falha ao atualizar o registro do filme" });
     }
 
     // retornar o status correto informando que o filme foi atualizado
     res.status(200).send();
+});
+
+app.delete("/movies/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    try {
+        const movie = await prisma.movie.findUnique({ where: { id } });
+
+        if (!movie) {
+            return res.status(404).send({ message: "O filme não foi encontrado" });
+        }
+
+        await prisma.movie.delete({ where: { id } });
+    } catch (error) {
+        return res.status(500).send({ message: "Não foi possível remover o filme" })
+    }
+
+    res.status(200).send();
+
 });
 
 app.listen(port, () => {
