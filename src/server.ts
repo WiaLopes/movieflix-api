c8c2dc501c2a79a5949944dcfae1a65752d47ac0
Willian Lopes <contato.willianlopes1@gmail.com>
2025-09-30 21:23:46 -0300

    adicionada atualização de filme

--- src/server.ts
+++ src/server.ts
@@ -29,10 +29,10 @@ app.post("/movies", async (req, res) => {
         // verificar no banco se já existe um filme com o nome que está sendo enviado
 
         const movieWithSameTitle = await prisma.movie.findFirst({
-            where: { title: {equals: title, mode: "insensitive"} },
+            where: { title: { equals: title, mode: "insensitive" } },
         });
 
-        if(movieWithSameTitle) {
+        if (movieWithSameTitle) {
             return res.status(409).send({ message: "Já existe um filme cadastrado com esse título" });
         }
 
@@ -46,11 +46,45 @@ app.post("/movies", async (req, res) => {
                 release_date: new Date(release_date)
             }
         });
-    }catch(error){
-        return res.status(500).send({message: "Falha ao cadastrar umm filme"});
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao cadastrar um filme" });
     }
 
     res.status(201).send();
+});
+
+// Como atualizar os dados de um filme?
+app.put("/movies/:id", async (req, res) => {
+    // pegar o id do registro que vai ser atualizado
+    const id = Number(req.params.id);
+
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: {
+                id
+            }
+        });
+
+        if (!movie) {
+            return res.status(404).send({ message: "Filme não encontrado" });
+        }
+
+        const data = { ...req.body };
+        data.release_date = data ? new Date(data.release_date) : undefined;
+
+        // pegar os dados do filmme que será atualizado
+        const movie = await prisma.movie.update({
+            where: {
+                id
+            },
+            data: data
+        });
+    }catch(error) {
+        return res.status(500).send({ message: "Falha ao atualizar o registro do filme" });
+    }
+
+    // retornar o status correto informando que o filme foi atualizado
+    res.status(200).send();
 });
 
 app.listen(port, () => {
